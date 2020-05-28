b73a1ab8b54d27b99980792eb6bec01b25e69a67
kadri <kge.sportcompass.dk>
2020-05-28 10:06:59 +0300

    Updates

---
+++ precache-manifest.c688969bae9d0fc5b453ca3ff6b6a285.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342507a76b491fa35a9024d5c9658063",
+    "url": "/punk-beer-app/index.html"
+  },
+  {
+    "revision": "daa7900b8e412ed97de1",
+    "url": "/punk-beer-app/static/css/main.ac4103fc.chunk.css"
+  },
+  {
+    "revision": "af1538222022d91402c0",
+    "url": "/punk-beer-app/static/js/2.d6ab237b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/punk-beer-app/static/js/2.d6ab237b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daa7900b8e412ed97de1",
+    "url": "/punk-beer-app/static/js/main.c3853d40.chunk.js"
+  },
+  {
+    "revision": "917a65a2a0ac4cfa2b30",
+    "url": "/punk-beer-app/static/js/runtime-main.20e4d85e.js"
+  }
+]);
